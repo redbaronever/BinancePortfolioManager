e545f556faa7603f7a83dea18d7224004dbb57ae
redbaronever <redbaronever@gmail.com>
2021-10-16 02:49:27 +0700

    add new source

---
+++ src/main/java/hnt/com/BinancePortfolioApplication.java
@@ -0,0 +1,13 @@
+package hnt.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinancePortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinancePortfolioApplication.class, args);
+	}
+
+}
